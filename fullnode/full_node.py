0e542a75fbe9217b01ece4d75343fe6743fa51d7
kckc0608 <kckc0608@naver.com>
2024-11-18 14:07:49 +0900

    feat: 소켓 통신 기능

--- fullnode/full_node.py
+++ fullnode/full_node.py
@@ -1,8 +1,45 @@
+from collections import deque
+import socket
+from threading import Thread
+
 class FullNode():
+    HOST = '127.0.0.1'
+    PORT = 9999
 
-    def __init__(self):
-        pass
+    def __init__(self, transactions, utxo_set):
+        self.transactions = transactions
+        self.utxo_set = utxo_set
+        self.query_queue = deque()
 
-    def __load_transactions(self):
+        self.__set_socket()
+        query_listening_thread = Thread(target=self.__get_query, daemon=True)
+        query_listening_thread.start()
 
-        pass
+
+    def run(self):
+        while True:
+            if self.query_queue:
+                query = self.query_queue.popleft()
+                print(query)
+                # process query
+                # send response
+                continue
+            # process transaction
+
+    def __set_socket(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.HOST, self.PORT))
+
+
+    def __get_query(self):
+        self.server_socket.listen()
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            data = str(client_socket.recv(1024), encoding='utf-8')
+            self.query_queue.append(data)
+            print("receive query")
+
+
+node = FullNode(None, None)
+node.run()
--- query_process.py
+++ query_process.py
@@ -1,6 +1,10 @@
+import socket
+
 class QueryProcess:
 
     def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(('127.0.0.1', 9999))
         pass
 
     def run(self):
@@ -15,8 +19,14 @@ class QueryProcess:
             if len(tokens) == 2:
                 if tokens[0] == 'snapshot':
                     if tokens[1] == 'transactions':
+                        self.client_socket.send(bytes(tokens[0], 'utf-8'))
+                        data = self.client_socket.recv(1024)
+                        print(data)
                         continue
                     if tokens[1] == 'utxoset':
                         continue
 
             print("알 수 없는 명령어 입니다.")
+
+process = QueryProcess()
+process.run()
